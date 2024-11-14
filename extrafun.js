f242c7a8c6b217fdaebb19d6a139fd3887f35133
jeffmoore <jmo342006@gmail.com>
2024-11-14 16:21:22 -0500

    Made overall style changes and added show and hide email

---
+++ extrafun.js
@@ -0,0 +1,14 @@
+function SEmail() {
+    var x = document.getElementById('email');
+    if (x.style.display == 'none') {
+      x.style.display = 'block';
+    } else  {
+      x.style.display = 'none';
+    }
+  }
+  
+  function HideEmail() {
+    var x = document.getElementById('email');
+    x.style.display = 'none';
+    
+  }
